40d721f144966ae47ea89adb2526b67d6168a23f
meh <meh@paranoici.org>
2012-07-28 18:48:35 +0200

    examples: reintrodue the object oriented stuff

--- examples/echo.rb
+++ examples/echo.rb
@@ -2,9 +2,51 @@
 require 'rubygems'
 require 'barlume'
 
-GC.disable
+class Echo < Barlume::Lucciola
+	CHUNK_SIZE = 2048
 
-lanterna = Barlume::Lanterna.poll
+	def initialize (*)
+		super
+		
+		@buffer = ''
+	end
+
+	def read_all
+		while chunk = read(CHUNK_SIZE)
+			@buffer << chunk
+		end
+	rescue Errno::EAGAIN; end
+
+	def write_all
+		while @buffer.length > 0
+			if @buffer.length <= CHUNK_SIZE
+				written = write @buffer
+
+				if written < @buffer.length
+					@buffer[0, written] = ''
+
+					return false
+				else
+					@buffer.clear
+				end
+			else
+				written = write @buffer[0, CHUNK_SIZE]
+
+				@buffer[0, written] = ''
+
+				if written < CHUNK_SIZE
+					return false
+				end
+			end
+		end
+
+		true
+	rescue Errno::EAGAIN
+		false
+	end
+end
+
+lanterna = Barlume::Lanterna.best
 server   = lanterna.add(TCPServer.new(1337)).asynchronous!.readable!
 server.listen(1024)
 
@@ -15,19 +57,22 @@ loop do
 		if event == :error
 			lucciola.delete!.close rescue nil
 		elsif event == :readable
-			begin
-				if lucciola == server
-					while client = lucciola.accept
-						lanterna.add(client).asynchronous!.readable!
-					end
-				else
-					begin
-						lucciola.write lucciola.read(2048)
-					rescue EOFError, Errno::ECONNRESET
-						lucciola.delete!.close
-					end
+			if lucciola == server
+				while client = lucciola.accept rescue nil
+					lanterna.add(Echo.new(client)).asynchronous!.readable!
 				end
-			rescue Errno::EAGAIN; end
+			else
+				begin
+					lucciola.read_all
+					lucciola.writable!
+				rescue EOFError, Errno::ECONNRESET
+					lucciola.delete!
+				end
+			end
+		elsif event == :writable
+			if lucciola.write_all
+				lucciola.no_writable!
+			end
 		end
 	}
 end
