01a949282730314f33c59ddee296b1e69aff1225
meh <meh@schizofreni.co>
2012-10-22 19:23:27 +0200

    Fix up license headers

--- lib/barlume/lanterna/port.rb
+++ lib/barlume/lanterna/port.rb
@@ -1,7 +1,7 @@
 #--
-# Copyleft meh. [http://meh.paranoid.pk | meh@paranoici.org]
+# Copyleft (ɔ) meh. - http://meh.schizofreni.co
 #
-# This file is part of barlume.
+# This file is part of barlume - https://github.com/meh/barlume
 #
 # barlume is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
